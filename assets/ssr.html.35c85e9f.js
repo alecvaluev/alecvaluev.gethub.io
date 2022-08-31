b70eb4722c581e52a49d31b7d246a36e9b01d4ac
alecvaluev <alecvaluev@users.noreply.github.com>
2022-08-31 02:54:34 +0000

    deploy: f9b209aed5c685db677bd8b2a80a0f00edc022c4

---
+++ assets/ssr.html.35c85e9f.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-cb3cb9a0","path":"/dev/Next/ssr.html","title":"SSR: Server-side rendering","lang":"en-US","frontmatter":{},"excerpt":"","headers":[{"level":2,"title":"Usage","slug":"usage","link":"#usage","children":[]}],"git":{"updatedTime":1661914449000},"filePathRelative":"dev/Next/ssr.md"}');export{e as data};
